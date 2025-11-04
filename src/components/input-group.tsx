86e124a76b682ed5875168604ebfee9be10c0a60
sina-byn <sina.sina.bayan@protonmail.com>
2025-11-04 20:17:52 +0330

    feat: input-group component impl

---
+++ src/components/input-group.tsx
@@ -0,0 +1,43 @@
+import { cn } from '../lib/utils';
+
+export type InputGroupProps = React.ComponentProps<'div'>;
+
+export function InputGroup({ className, children, ...props }: InputGroupProps) {
+  return (
+    <div
+      role='group'
+      className={cn(
+        'flex flex-wrap focus-within:outline **:outline-none gap-1',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export type InputGroupAddonProps = React.ComponentProps<'div'> & {
+  block?: boolean;
+};
+
+export function InputGroupAddon({
+  block = false,
+  className,
+  children,
+  ...props
+}: InputGroupAddonProps) {
+  return (
+    <div
+      role='group'
+      className={cn(
+        'inline-flex items-center gap-1 shrink-0',
+        block && 'flex w-full',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
