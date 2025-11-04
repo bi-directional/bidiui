09e2fa10f990ecfe009234b032065dcbebe6eb34
sina-byn <sina.sina.bayan@protonmail.com>
2025-11-04 20:11:29 +0330

    chore: toPlain util fn impl

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,9 @@ import { twMerge } from 'tailwind-merge';
 import clsx, { type ClassArray } from 'clsx';
 
 export const cn = (...inputs: ClassArray) => twMerge(clsx(inputs));
+
+export const toPlain = (n: number | string) => {
+  return typeof n === 'number'
+    ? n.toLocaleString('fullwide', { useGrouping: false })
+    : n;
+};
